35595955d77b68a579607fcd8ad1c9508fa1508d
박승은 <noqo23@naver.com>
2022-12-05 20:51:39 +0900

    springboot

--- src/main/java/com/ti/ty/controller/BoardController.java
+++ src/main/java/com/ti/ty/controller/BoardController.java
@@ -1,9 +1,13 @@
 package com.ti.ty.controller;
 
+import com.sun.org.apache.xpath.internal.operations.Mod;
 import com.ti.ty.model.Board;
 import com.ti.ty.repository.BoardRepository;
 import com.ti.ty.service.BoardService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -20,9 +24,17 @@ public class BoardController {
     private BoardRepository boardRepository;
 
     @GetMapping("/board/list")
-    public String list(Model model) {
+    public String list(Model model,@PageableDefault(size = 5) Pageable pageable,
+                       @RequestParam(required = false, defaultValue = "") String searchText) {
 
-        model.addAttribute("boards", boardService.boardList());
+//        Page<Board> boards = boardRepository.findAll(pageable);
+        Page<Board> boards = boardRepository.findByTitleContainingOrContentContaining(searchText,
+                searchText, pageable);
+        int startPage = 1;
+        int endPage = boards.getTotalPages();
+        model.addAttribute("startPage",startPage);
+        model.addAttribute("endPage",endPage);
+        model.addAttribute("boards", boards);
         return "board/list";
     }
 
@@ -42,6 +54,16 @@ public class BoardController {
     public String boardWrite(@ModelAttribute Board board) {
         boardService.write(board);
         return "redirect:/board/list";
+    }
+
+    @GetMapping("/board/boardList/{id}")
+    public String boardList(@PathVariable Long id, Model model){
+
+        Board board = boardRepository.findById(id).orElse(null);
+
+        model.addAttribute("boards", board);
+
+        return "/board/boardList";
     }
 
 
--- src/main/java/com/ti/ty/service/BoardService.java
+++ src/main/java/com/ti/ty/service/BoardService.java
@@ -3,6 +3,9 @@ package com.ti.ty.service;
 import com.ti.ty.model.Board;
 import com.ti.ty.repository.BoardRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -13,10 +16,6 @@ public class BoardService {
     @Autowired
     private BoardRepository boardRepository;
 
-    public List<Board> boardList(){
-
-        return boardRepository.findAll();
-    }
 
     public void write(Board board){
 
